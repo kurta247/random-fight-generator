cc86552b0d93a336e1e29210385b960a80665b60
Kurtana <147049059+kurta247@users.noreply.github.com>
2024-12-22 04:58:44 -0800

    Update random.fight.generator.py

--- random.fight.generator.py
+++ random.fight.generator.py
@@ -1,6 +1,6 @@
 import random
 fighter1 = input("enter the first fighter: ")
 fighter2 = input('enter the second fighter: ')
-BY = random.choice(['unanimous decision', 'split decision', 'TKO', 'submission'])
+BY = random.choice(['unanimous decision', 'split decision', 'TKO', 'submission', 'DQ'])
 winner = random.choice([fighter1, fighter2])
-print(f'And winner by', BY, 'is:', winner )
+print(f'And the winner by', BY, 'is:', winner )
